331ea0c2640e6d017ea59e7e88ca4435165639fc
Demi Ben-Ari <demi.benari@gmail.com>
2014-09-17 20:21:02 +0300

    Changes to the Add Task Controller

--- app/controllers/addTaskController.js
+++ app/controllers/addTaskController.js
@@ -4,6 +4,24 @@
 
 angular.module('tasksList').controller("AddTaskController", ['$scope', function($scope){
     // Saving the reference to the controller by a variable
-    var addTaskController = this;
-    addTaskController.list = dataService.data;
+//    var addTaskController = this;
+//    addTaskController.list = dataService.data;
+
+
+    // Saving the reference to the controller by a variable
+    var addTaskCtrl = this;
+
+    this.task = {};
+
+    $scope.isAddTaskShown = false;
+
+    addTaskCtrl.addTask = function() {
+       // alert(addTaskCtrl.task.name + " " + addTaskCtrl.task.assignee + " " + addTaskCtrl.task.description + " " + addTaskCtrl.task.date);
+        alert( $scope.taskList);
+        $scope.taskList.push(addTaskCtrl.task);
+    };
+
+    addTaskCtrl.clearTask = function() {
+        this.task = {};
+    };
 }]);
--- app/controllers/taskListController.js
+++ app/controllers/taskListController.js
@@ -1,6 +1,9 @@
-angular.module('tasksList').controller("TaskListController", ['dataService', function(dataService){
+angular.module('tasksList').controller("TaskListController", ['$scope','dataService', function($scope, dataService){
     // Saving the reference to the controller by a variable
     var taskListCtrl = this;
 
-    taskListCtrl.list = dataService.data;
+    $scope.taskList = dataService.data;
+
+    alert($scope.taskList);
+//    taskListCtrl.list = dataService.data;
 }]);
